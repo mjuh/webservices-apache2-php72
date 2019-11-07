42518fdcad7061fa1fd573388e1eebc2c1d62fb3
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-07 19:52:55 +0300

    default.nix: Use nss-certs instead of cacert.

--- default.nix
+++ default.nix
@@ -44,7 +44,7 @@ pkgs.dockerTools.buildLayeredImage rec {
     coreutils
     libjpeg_turbo
     (optipng.override{ inherit libpng ;})
-    gifsicle cacert zip
+    gifsicle nss-certs.unbundled zip
   ]
   ++ collect isDerivation phpPackages.php72Packages
   ++ collect isDerivation mjperl5Packages;
