476069a28f639a0bb4062d7b8d158685b34a062c
Oleg Pykhalov <go.wigust@gmail.com>
2019-11-12 20:27:01 +0300

    default.nix: Replace postfix with sendmail.

--- default.nix
+++ default.nix
@@ -19,7 +19,8 @@ let
     name = "apache2-rootfs-php72";
     src = ./rootfs;
     inherit curl coreutils findutils apacheHttpdmpmITK apacheHttpd
-      mjHttpErrorPages postfix s6 execline;
+      mjHttpErrorPages s6 execline;
+                                   postfix = sendmail;
     php72 = php.php72;
     mjperl5Packages = mjperl5lib;
     ioncube = ioncube.v72;
@@ -48,7 +49,7 @@ pkgs.dockerTools.buildLayeredImage rec {
     rootfs
     tzdata
     locale
-    postfix
+                                  sendmail
     sh
     coreutils
     libjpeg_turbo
