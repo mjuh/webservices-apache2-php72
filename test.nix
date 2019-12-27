4c2b187818559be7bcfe1cf9c7a850dca79c84a3
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-27 16:38:12 +0300

    test.nix: Delete defaultTestSuite.

--- test.nix
+++ test.nix
@@ -22,7 +22,6 @@ in maketestPhp {
   php = php72;
   inherit containerStructureTestConfig;
   rootfs = ./rootfs;
-  defaultTestSuite = false;
   testSuite = [
     (dockerNodeTest {
       description = "Copy phpinfo.";
