765d3eb49ae429346c9ae74d28cd50509a1d3091
Oleg Pykhalov <go.wigust@gmail.com>
2020-03-02 10:51:02 +0300

    test.nix: Rename deepdiff from html to json.

--- test.nix
+++ test.nix
@@ -67,7 +67,7 @@ in maketestPhp {
       command = testDiffPy {
         inherit pkgs;
         sampleJson = (./tests/. + "/${phpVersion}.json");
-        output = "/tmp/xchg/coverage-data/deepdiff-with-excludes.html";
+        output = "/tmp/xchg/coverage-data/deepdiff-with-excludes.json";
         excludes = import ./tests/diff-to-skip.nix;
       };
     })
@@ -77,7 +77,7 @@ in maketestPhp {
       command = testDiffPy {
         inherit pkgs;
         sampleJson = (./tests/. + "/web34/${phpVersion}.json");
-        output = "/tmp/xchg/coverage-data/deepdiff-web34.html";
+        output = "/tmp/xchg/coverage-data/deepdiff-web34.json";
       };
     })
     (dockerNodeTest {
@@ -86,7 +86,7 @@ in maketestPhp {
       command = testDiffPy {
         inherit pkgs;
         sampleJson = (./tests/. + "/web34/${phpVersion}.json");
-        output = "/tmp/xchg/coverage-data/deepdiff-web34-with-excludes.html";
+        output = "/tmp/xchg/coverage-data/deepdiff-web34-with-excludes.json";
         excludes = import ./tests/diff-to-skip.nix;
       };
     })
@@ -202,7 +202,7 @@ in maketestPhp {
     (dockerNodeTest {
       description = "deepdiff iterable_item_removed";
       action = "succeed";
-      command = "jq .iterable_item_removed /tmp/xchg/coverage-data/deepdiff-with-excludes.html ; jq .iterable_item_removed /tmp/xchg/coverage-data/deepdiff-with-excludes.html | grep null ";
+      command = "jq .iterable_item_removed /tmp/xchg/coverage-data/deepdiff-with-excludes.json ; jq .iterable_item_removed /tmp/xchg/coverage-data/deepdiff-with-excludes.json | grep null ";
     })
   ];
 } { }
