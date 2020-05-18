cc3228f0f515758a764e89c8f43a71759f3e4134
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-18 20:01:34 +0300

    default.nix: Pin nixpkgs.

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ overlayUrl ? "git@gitlab.intr:_ci/nixpkgs.git"
-, overlayRef ? "master" }:
+{ nixpkgs ? (import <nixpkgs> { }).fetchgit {
+  url = "https://github.com/NixOS/nixpkgs.git";
+  rev = "ce9f1aaa39ee2a5b76a9c9580c859a74de65ead5";
+  sha256 = "1s2b9rvpyamiagvpl5cggdb2nmx4f7lpylipd397wz8f0wngygpi";
+}, overlayUrl ? "git@gitlab.intr:_ci/nixpkgs.git", overlayRef ? "master" }:
 
-with import <nixpkgs> {
+with import nixpkgs {
   overlays = [
     (import (builtins.fetchGit { url = overlayUrl; ref = overlayRef; }))
   ];
