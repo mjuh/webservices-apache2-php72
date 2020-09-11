8b474207c01f62ab2d5cbdb9d34d3e22265ec254
Oleg Pykhalov <go.wigust@gmail.com>
2020-09-11 18:05:42 +0300

    Pass only nixpkgs.

--- default.nix
+++ default.nix
@@ -1,14 +1,13 @@
-{ nixpkgs ? (import <nixpkgs> { }).fetchgit {
-  url = "https://github.com/NixOS/nixpkgs.git";
-  rev = "ce9f1aaa39ee2a5b76a9c9580c859a74de65ead5";
-  sha256 = "1s2b9rvpyamiagvpl5cggdb2nmx4f7lpylipd397wz8f0wngygpi";
-}, overlayUrl ? "git@gitlab.intr:_ci/nixpkgs.git", overlayRef ? "master" }:
-
-with import nixpkgs {
+{ nixpkgs ? import <nixpkgs> {
   overlays = [
-    (import (builtins.fetchGit { url = overlayUrl; ref = overlayRef; }))
+    (import (builtins.fetchGit {
+      url = "git@gitlab.intr:_ci/nixpkgs.git";
+      ref = "master";
+    }))
   ];
-};
+} }:
+
+with nixpkgs;
 
 let
   inherit (builtins) concatMap getEnv toJSON;
