e7df2972e5b3173a2d7223fd7f17ef56c65de581
Alexander Rezvov <rezvov@majordomo.ru>
2021-04-23 13:54:53 +0300

    Fix svgo symlink

--- default.nix
+++ default.nix
@@ -79,6 +79,8 @@ pkgs.dockerTools.buildLayeredImage rec {
       mkdir -p bin
       mkdir -p usr/local
       mkdir -p opt
+      chmod 755 bin
+      ln -s ${nodePackages.svgo}/bin/svgo bin/svgo
       ln -s ${php72} opt/php72
       ln -s /bin usr/bin
       ln -s /bin usr/sbin
